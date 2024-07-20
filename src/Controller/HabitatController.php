716ebd67f974c52cbdceb93d48ca8ecb29394498
Ehoungban betsy <betsy@vs-invest.group>
2024-07-20 06:57:14 +0200

    création des crud

---
+++ src/Controller/HabitatController.php
@@ -0,0 +1,88 @@
+<?php
+
+// src/Controller/HabitatController.php
+namespace App\Controller;
+
+use App\Entity\Habitat;
+use App\Form\HabitatType;
+use App\Repository\HabitatRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/habitat')]
+class HabitatController extends AbstractController
+{
+    #[Route('/', name: 'habitat_index', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    public function index(HabitatRepository $habitatRepository): Response
+    {
+        return $this->render('habitat/index.html.twig', [
+            'habitats' => $habitatRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'habitat_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $habitat = new Habitat();
+        $form = $this->createForm(HabitatType::class, $habitat);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($habitat);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('habitat_index');
+        }
+
+        return $this->renderForm('habitat/new.html.twig', [
+            'habitat' => $habitat,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'habitat_show', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    public function show(Habitat $habitat): Response
+    {
+        return $this->render('habitat/show.html.twig', [
+            'habitat' => $habitat,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'habitat_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function edit(Request $request, Habitat $habitat, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(HabitatType::class, $habitat);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('habitat_index');
+        }
+
+        return $this->renderForm('habitat/edit.html.twig', [
+            'habitat' => $habitat,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'habitat_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function delete(Request $request, Habitat $habitat, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$habitat->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($habitat);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('habitat_index');
+    }
+}
---
+++ src/Controller/ServiceController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Service;
+use App\Form\ServiceType;
+use App\Repository\ServiceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/service')]
+class ServiceController extends AbstractController
+{
+    #[Route('/', name: 'service_index', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    public function index(ServiceRepository $serviceRepository): Response
+    {
+        return $this->render('service/index.html.twig', [
+            'services' => $serviceRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'service_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $service = new Service();
+        $form = $this->createForm(ServiceType::class, $service);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($service);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('service_index');
+        }
+
+        return $this->renderForm('service/new.html.twig', [
+            'service' => $service,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'service_show', methods: ['GET'])]
+    #[IsGranted('ROLE_USER')]
+    public function show(Service $service): Response
+    {
+        return $this->render('service/show.html.twig', [
+            'service' => $service,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'service_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function edit(Request $request, Service $service, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ServiceType::class, $service);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('service_index');
+        }
+
+        return $this->renderForm('service/edit.html.twig', [
+            'service' => $service,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'service_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function delete(Request $request, Service $service, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$service->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($service);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('service_index');
+    }
+}
